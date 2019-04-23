5ce0502c46f17b4893fcb65bd7563212bbbe61b5
jian.li <jian.li@dmall.com>
2019-04-23 09:35:41 +0800

    框架优化，集成sequelize

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-if __name__ == '__main__':
-    print("http://127.0.0.1:9090")
-    app.run(host='0.0.0.0',port=9090,debug=True)
